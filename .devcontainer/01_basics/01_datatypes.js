f1a17a6a3afe911ee235e3fdff812c30a4bcab81
karandeepsingh120 <122249968+karandeepsingh120@users.noreply.github.com>
2026-09-07 02:34:35 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all ja code as newer version
+
+alert("hello") // we are using nodejs not browser
+console.log(3+3) //code readability should be high
+
+let name = "karan"
+let age = 20
+let isLoggedIn = true
+
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value eg let state = null
+// undefined => //eg let state;
+// symbol => unique
+
+
+// object
+
+console.log(typeof "karan");//string
+console.log(typeof null); // object
