e8517bef9f28c5def48bfa3432b8053934a842ad
karandeepsingh120 <122249968+karandeepsingh120@users.noreply.github.com>
2026-09-07 02:55:37 +0000

    datatypes

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -3,5 +3,8 @@ let accountemail = "karan@gmail.com"
 var accountpasswrd = "5631"
 accountcity = "jaipur"
 
-accountId = 1234
+// accountId = 1234 // notaloowed
+accountemail = "deep@n.com"
+accountpasswrd = "2121"
 console.log(accountId);
+console.table([accountemail,accountpasswrd])
